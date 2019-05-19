b0312bb7ea5789123a186034efea4f2d3f5c2249
Junyu Guo <GuoJunyu@iMac.local>
2019-05-19 14:17:15 -0700

    add new projects

--- max_subarray.py
+++ max_subarray.py
@@ -30,12 +30,13 @@ class Solution:
     def maxSubArray(self, nums):
         # write your code here
         max_sum = -sys.maxsize
-        min_sum = 0  # minimum sum from 0 to position k (k<=current)
+        min_sum = 0  # minimum sum from 0 to position k (k<=current)， initially must be 0：只有一个数的例子
         prefix_sum = 0  # sum from position 0 to current
         for i in range(len(nums)):
             prefix_sum += nums[i]
             max_sum = max(max_sum, prefix_sum-min_sum)
             min_sum = min(prefix_sum, min_sum)
         return max_sum
+# 注意要先更新maxsum 然后更新minsum： minsum 是前几个数的最小和
 
 
--- merge_k_soted_linked_lists.py
+++ merge_k_soted_linked_lists.py
@@ -1,4 +1,13 @@
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
 import heapq
+
 
 class Solution:
     """
@@ -9,22 +18,25 @@ class Solution:
     def mergeKLists(self, lists):
         # write your code here
         heap = []
-        for node in lists:
-            if node is not None:
-                heapq.heappush(heap, (node.val, node))
+        count = 0 # 这里面加count 是为了 break tie
+        for list in lists:
+            if list: # list 存在的时候才加
+                heapq.heappush(heap, (list.val, count, list))
+                count += 1
 
-        dummy = ListNode(0)
-        head = dummy
-        while len(heap) != 0:
-            val, node = heapq.heappop(heap)
-            dummy.next = node
-            if node.next is not None:
-                heapq.heappush(heap, (node.next.val, node.next))
-            dummy = dummy.next
+        dummy = ListNode(-1)
+        temp = dummy
+        while heap:
+            _, _, node = heapq.heappop(heap)
+            if node.next: # next 存在的时候才加
+                heapq.heappush(heap, (node.next.val, count, node.next))
+            temp.next = node
+            temp = temp.next
+            count += 1
+        return dummy.next
 
-        return head.next
 
 
-# use heap. in the begining add all heads of the lists to the heap.
+        # use heap. in the beginning add all heads of the lists to the heap.
 # then when heap is not empty, pop from the heap (first pop out the smallest value) (note here we can add more than 1 dimension (node.val, node)to the heap)
 # after pop, add the next node in to the heap
